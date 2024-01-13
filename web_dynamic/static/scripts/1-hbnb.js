e37456336f449f7f12ffa9d7a48ffe0f873a804b
Olisajioke <chijioke914@gmail.com>
2024-01-14 00:42:06 +0100

    updated for task 3 and 2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,15 +1,16 @@
-ddEventListener('DOMContentLoaded', function () {
-  $(function () {
-    const myDict = {};
-    $('input[type="checkbox"]').click(function () {
-      if ($(this).is(':checked')) {
-        myDict[$(this).attr('data-id')] = $(this).attr('data-name');
-        console.log($(this).attr('data-id'));
-        $('.amenities h4').text(Object.values(myDict).join(', '));
-      } else if ($(this).is(':not(:checked)')) {
-        delete myDict[$(this).attr('data-id')];
-        $('.amenities h4').text(Object.values(myDict).join(', '));
-      }
-    });
+window.addEventListener('load', function () {
+  // task 2
+  const amenityIds = {};
+  $('input[type=checkbox]').change(function () {
+    if ($(this).prop('checked')) {
+      amenityIds[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else if (!$(this).prop('checked')) {
+      delete amenityIds[$(this).attr('data-id')];
+    }
+    if (Object.keys(amenityIds).length === 0) {
+      $('div.amenities h4').html('&nbsp');
+    } else {
+      $('div.amenities h4').text(Object.values(amenityIds).join(', '));
+    }
   });
 });
